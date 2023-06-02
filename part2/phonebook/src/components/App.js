19acd696059c1636f4f946ee0f53b0b3db24aff8
Lance Ziegler <lance.ziegler@hotmail.com>
2023-06-02 11:01:53 -0700

    edited phonebook app js

---
+++ part2/phonebook/src/components/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useState } from 'react';
+
+//comment
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+    { name: 'Lance Ziegler' },
+  ]);
+  const [newName, setNewName] = useState('');
+
+  const addPerson = (e) => {
+    e.preventDefault();
+
+    const nameExists = persons.some((person) => person.name === newName);
+
+    if (nameExists) {
+      alert(`${newName} is already in the phonebook!`);
+    } else {
+      const personObject = { name: newName };
+      setPersons(persons.concat(personObject));
+    }
+
+    setNewName('');
+  };
+
+  const handlePersonChange = (e) => {
+    console.log(e.target.value);
+    setNewName(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
